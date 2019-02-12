75fb985981b014f06931413b12aa364141a9b1e9
Sobhan Mondal <30654420+sobhaniiest@users.noreply.github.com>
2019-02-12 19:31:39 +0530

    Add files via upload

---
+++ ftp_c.c
@@ -0,0 +1,280 @@
+//Inro - 12/02/2019
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>	
+#include <sys/types.h>
+#include <sys/socket.h>						
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// Server PORT
+#define PSC 1024
+#define PSF 1025
+#define PRAND 1026
+
+// Client PORT
+#define PCC 2024
+#define PCF 2025
+
+#define SIZE 100
+
+/*
+ls
+pwd
+put
+get
+cd
+mkdir
+bye
+*/
+
+char str[SIZE];
+FILE *fp;
+
+void fun(int newcmd, int newdata)
+{
+	//printf("\n\n\n\n\n");
+	printf("Connected to 127.0.0.10.\n");
+	printf("Name : ");
+	bzero(str, sizeof(str)); 
+    fgets(str, sizeof(str), stdin);
+	write(newcmd, str, sizeof(str)); 
+	bzero(str, sizeof(str)); 
+    read(newcmd, str, sizeof(str)); 
+    if(!strcmp(str,"i"))
+    {
+    	printf("User Name Okay ;-), Enter Password...\nPassword : ");
+    	bzero(str, sizeof(str)); 
+	    fgets(str, sizeof(str), stdin);
+		write(newcmd, str, sizeof(str)); 
+		bzero(str, sizeof(str)); 
+	    read(newcmd, str, sizeof(str));
+	    if(!strcmp(str,"r"))
+	    	printf("User logged in, Yooo!!! ;-)\n");
+	    else
+	    {
+	    	printf("Incorrect Password :-(\n");
+	    	exit(0);
+	    }
+
+    }
+    else
+    {
+    	printf("Incorrect User ID :-(\n");
+    	exit(0);
+    }
+
+    //////////////////////////////////////////////////////////
+    int i,k,m,flag;
+    char ch,file[SIZE],com[SIZE];
+    
+    while(1)
+    {
+    	printf("ftp>");
+    	bzero(str, sizeof(str));
+    	fgets(str, sizeof(str), stdin);
+    	str[strlen(str)-1] = '\0';
+
+    	k = 0;
+    	m = 0;
+    	flag = 0;
+    	for(i=0;i<strlen(str);i++)
+    	{
+    		if(flag == 1)
+    			file[k++] = str[i];
+    		if(str[i] == ' ')
+    			flag = 1;
+    		if(flag == 0)
+    			com[m++] = str[i];
+    	}
+    	file[k++] = '\0';
+    	com[m++] = '\0';
+
+ 		if(flag == 0)
+ 		{
+ 			//printf("%s\n",com);
+ 			if(!strcmp(com,"bye"))
+ 				exit(0);
+ 			else if(!strcmp(com,"pwd"))
+ 			{
+ 				write(newcmd, str, sizeof(str));
+				while(1)
+	 			{
+	 				recv(newdata, &ch, sizeof(char),0);
+	 				if(ch == EOF)
+	 					break;
+	 				printf("%c",ch);
+	 			}
+ 			}
+ 			else if(!strcmp(com,"ls"))
+ 			{
+ 				write(newcmd, str, sizeof(str));
+				while(1)
+	 			{
+	 				recv(newdata, &ch, sizeof(char),0);
+	 				if(ch == EOF)
+	 					break;
+	 				printf("%c",ch);
+	 			}
+ 			}
+ 			else if(!strcmp(com,"\0"))
+ 				continue;
+ 			else
+ 				printf("Command Not Found!!!\n");
+
+ 		}
+ 		else
+ 		{
+ 			//printf("%s %s\n",com,file);
+ 			if(!strcmp(com,"put"))
+ 			{ 
+ 				fp = fopen(file,"r");
+ 				if(fp == NULL)
+ 				{
+ 					printf("%s doesn't exist!!!\n",file);
+ 					continue;
+ 				}
+ 				else
+ 				{
+ 					write(newcmd, str, sizeof(str));
+ 					while(ch=fgetc(fp))
+ 					{
+ 						send(newdata, &ch, sizeof(char),0);
+ 						if(ch == EOF)
+ 							break;
+ 					}
+ 					fclose(fp);
+ 					printf("File Transfer Complete!!!\n");
+ 				}
+
+ 			}
+ 			else if(!strcmp(com,"get"))
+ 			{
+ 				write(newcmd, str, sizeof(str));
+ 				bzero(str, sizeof(str)); 
+	    		read(newdata, str, sizeof(str));
+	    		if(!strcmp(str,"n"))
+	    		{
+	    			printf("%s doesn't exist!!!\n",file);
+ 					continue;
+	    		}
+	    		else
+	    		{
+	    			fp = fopen(file,"w");
+					while(1)
+	 				{
+	 					recv(newdata, &ch, sizeof(char),0);
+	 					if(ch == EOF)
+	 						break;
+	 					fprintf(fp,"%c",ch);
+	 				}
+	 				fclose(fp);
+	 				printf("File is received!!!\n");
+	    		}
+
+ 			}
+ 			else if(!strcmp(com,"cd"))
+ 			{
+ 				write(newcmd, str, sizeof(str));
+ 				bzero(str, sizeof(str));
+ 				read(newdata, str, sizeof(str));
+ 				printf("%s\n",str);
+ 			}
+ 			else if(!strcmp(com,"mkdir"))
+ 			{
+ 				write(newcmd, str, sizeof(str));
+ 				bzero(str, sizeof(str));
+ 				read(newdata, str, sizeof(str));
+ 				printf("%s\n",str);
+ 			}
+ 			else
+ 				printf("Command Not Found!!!\n");
+ 		}
+    }
+}
+
+/*
+void fun(int newcmd, int newdata)
+{
+	char str[SIZE];
+
+		while(1)
+		{
+			bzero(str, sizeof(str)); 
+		    printf("Client : "); 
+		    fgets(str, sizeof(str), stdin);
+		    write(newcmd, str, sizeof(str)); 
+		    bzero(str, sizeof(str)); 
+		    read(newdata, str, sizeof(str)); 
+		    printf("Server >> %s", str); 
+		}
+}
+*/
+int main()
+{
+
+		struct sockaddr_in ccmd, cdata;
+
+		//cmd
+		bzero((char *)&ccmd, sizeof(ccmd));
+		ccmd.sin_family = AF_INET;
+		ccmd.sin_addr.s_addr = inet_addr("127.0.0.10");
+		ccmd.sin_port = htons(PCC);
+
+		//data
+		bzero((char *)&cdata, sizeof(cdata));
+		cdata.sin_family = AF_INET;
+		cdata.sin_addr.s_addr = inet_addr("127.0.0.10");
+		cdata.sin_port = htons(PCF);
+
+		int cmd,data;
+
+		//////////////////cmd///////////////////
+
+		if((cmd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		{
+			perror("Can not create Socket for cmd");
+			exit(0);
+		}
+		else
+			printf("Socket is created for cmd.....\n");
+
+		ccmd.sin_port = htons(PSC);
+		
+		if((connect(cmd, (struct sockaddr *)&ccmd, sizeof(ccmd))) < 0)
+		{
+			perror("Connect is nt working for cmd");
+			exit(0);
+		}
+		else
+			printf("cmd Connecting.....\n");
+
+		/////////////data////////////////
+
+		if((data = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		{
+			perror("Can not create Socket for data");
+			exit(0);
+		}
+		else
+			printf("Socket is created for data.....\n");
+
+		cdata.sin_port = htons(PRAND);
+		
+		if((connect(data, (struct sockaddr *)&cdata, sizeof(cdata))) < 0)
+		{
+			perror("Connect is not working for data");
+			exit(0);
+		}
+		else
+			printf("data Connecting.....\n");
+
+		////////////////////////////////////
+
+		fun(cmd,data);
+
+		return 0;
+}
---
+++ ftp_s.c
@@ -0,0 +1,334 @@
+//Inro - 12/02/2019
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>	
+#include <sys/types.h>
+#include <sys/socket.h>						
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// Server PORT
+#define PSC 1024
+#define PSF 1025
+#define PRAND 1026
+
+// Client PORT
+#define PCC 2024
+#define PCF 2025
+
+#define SIZE 100
+
+char str[SIZE];
+FILE *fp;
+
+void fun(int newcmd, int newrand)
+{
+	//printf("\n\n\n\n\n");
+
+	bzero(str, SIZE); 
+	read(newcmd, str, sizeof(str));
+	str[strlen(str)-1] = '\0';
+	//printf("%s, %ld\n",str,strlen(str));
+
+	if(!(strcmp(str,"inro")))
+	{
+		bzero(str, SIZE);
+		strcpy(str,"i");
+		write(newcmd, str, sizeof(str));
+	}
+	else
+	{
+		bzero(str, SIZE);
+		strcpy(str,"n");
+		write(newcmd, str, sizeof(str));
+	}
+
+	bzero(str, SIZE); 
+	read(newcmd, str, sizeof(str));
+	str[strlen(str)-1] = '\0';
+	//printf("%s, %ld\n",str,strlen(str));
+
+	if(!(strcmp(str,"joker")))
+	{
+		bzero(str, SIZE);
+		strcpy(str,"r");
+		write(newcmd, str, sizeof(str));
+	}
+	else
+	{
+		bzero(str, SIZE);
+		strcpy(str,"o");
+		write(newcmd, str, sizeof(str));
+	}
+
+	//////////////////////////////////////////
+	int i,k,m,flag;
+    char ch,file[SIZE],com[SIZE];
+
+	//int s = send(newcmd, &PRAND, sizeof(int),0);
+
+	while(1)
+	{
+		bzero(str, SIZE); 
+		read(newcmd, str, sizeof(str));
+
+		k = 0;
+    	m = 0;
+    	flag = 0;
+    	for(i=0;i<strlen(str);i++)
+    	{
+    		if(flag == 1)
+    			file[k++] = str[i];
+    		if(str[i] == ' ')
+    			flag = 1;
+    		if(flag == 0)
+    			com[m++] = str[i];
+    	}
+    	file[k++] = '\0';
+    	com[m++] = '\0';
+
+		if(flag == 0)
+		{
+			if(!strcmp(com,"ls"))
+			{
+				system("ls>temp.txt");
+				fp = fopen("temp.txt","r");
+				while(ch=fgetc(fp))
+ 				{
+ 					send(newrand, &ch, sizeof(char),0);
+ 					if(ch == EOF)
+ 						break;
+ 				}
+ 				fclose(fp);
+			}
+			else if(!strcmp(com,"pwd"))
+			{
+				system("pwd>temp.txt");
+				fp = fopen("temp.txt","r");
+				while(ch=fgetc(fp))
+ 				{
+ 					send(newrand, &ch, sizeof(char),0);
+ 					if(ch == EOF)
+ 						break;
+ 				}
+ 				fclose(fp);
+			}
+		}
+		else
+		{
+			if(!strcmp(com,"get"))
+			{
+				fp = fopen(file,"r");
+ 				if(fp == NULL)
+ 				{
+ 					bzero(str, SIZE);
+					strcpy(str,"n");
+					write(newrand, str, sizeof(str));
+ 					continue;
+ 				}
+ 				else
+ 				{
+ 					while(ch=fgetc(fp))
+ 					{
+ 						send(newrand, &ch, sizeof(char),0);
+ 						if(ch == EOF)
+ 							break;
+ 					}
+ 					fclose(fp);
+ 				}
+			}
+			else if(!strcmp(com,"put"))
+			{
+				fp = fopen(file,"w");
+				while(1)
+ 				{
+ 					recv(newrand, &ch, sizeof(char),0);
+ 					if(ch == EOF)
+ 						break;
+ 					fprintf(fp,"%c",ch);
+ 				}
+ 				fclose(fp);
+
+			}
+			else if(!strcmp(com,"cd"))
+			{
+				chdir(file);
+				bzero(str, sizeof(str));
+				strcpy(str,"Directory is changed");
+		  		write(newrand, str, sizeof(str));
+
+			}
+			else if(!strcmp(com,"mkdir"))
+			{
+				system(str);
+				bzero(str, sizeof(str));
+				strcpy(str,"New Directory is created");
+		  		write(newrand, str, sizeof(str));
+			}
+		}
+	}
+}
+
+/*
+void fun(int newcmd, int newrand)
+{
+	char str[SIZE];
+
+		while(1)
+		{ 
+		    bzero(str, SIZE); 
+		    read(newcmd, str, sizeof(str));  
+			printf("Client >> %s", str); 
+			printf("Server : "); 
+			bzero(str, SIZE); 
+			fgets(str, sizeof(str), stdin);
+			write(newrand, str, sizeof(str));  
+		}		
+}
+*/
+
+int main()
+{
+		struct sockaddr_in scmd, sdata, srand;
+
+		//cmd
+		bzero((char *)&scmd, sizeof(scmd));
+		scmd.sin_family = AF_INET;
+		scmd.sin_addr.s_addr = inet_addr("127.0.0.10");
+		scmd.sin_port = htons(PSC);
+
+		//file
+		bzero((char *)&sdata, sizeof(sdata));
+		sdata.sin_family = AF_INET;
+		sdata.sin_addr.s_addr = inet_addr("127.0.0.10");
+		sdata.sin_port = htons(PSF);
+
+		//rand
+		bzero((char *)&srand, sizeof(srand));
+		srand.sin_family = AF_INET;
+		srand.sin_addr.s_addr = inet_addr("127.0.0.10");
+		srand.sin_port = htons(PRAND);
+
+		int cmd,data,rand;
+		int b;
+
+		/////////////////cmd///////////////////
+
+		if((cmd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		{
+			perror("Can not create Socket for cmd");
+			exit(0);
+		}
+		else
+			printf("Socket is created for cmd...\n");
+
+		b = bind(cmd, (struct sockaddr *)&scmd, sizeof(scmd));
+		if(b < 0)
+		{
+			perror("Bind is not working for cmd");
+			exit(0);
+		}
+		else
+			printf("Socket is binded for cmd...\n");
+
+		///////////////////data////////////////////
+
+		if((data = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		{
+			perror("Can not create Socket for data");
+			exit(0);
+		}
+		else
+			printf("Socket is created for data...\n");
+
+		b = bind(data, (struct sockaddr *)&sdata, sizeof(sdata));
+		
+		if(b < 0)
+		{
+			perror("Bind is not working for data");
+			exit(0);
+		}
+		else
+			printf("Socket is binded for data...\n");
+
+		/////////////////rand//////////////////////
+
+		if((rand = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		{
+			perror("Can not create Socket for rand");
+			exit(0);
+		}
+		else
+			printf("Socket is created for rand.....\n");
+
+		b = bind(rand, (struct sockaddr *)&srand, sizeof(srand));
+		
+		if(b < 0)
+		{
+			perror("Bind is not working for rand");
+			exit(0);
+		}
+		else
+			printf("Socket is binded for rand.....\n");
+
+		///////////////////////////////////////////
+
+		///////////////////////////////////////////
+
+		listen(cmd, 3);
+		listen(data, 3);
+		listen(rand, 3);
+
+		int lcmd = sizeof(scmd);
+		int ldata = sizeof(sdata);
+		int lrand = sizeof(srand);
+
+		int ndata,ncmd,nrand,pid;
+		
+		scmd.sin_port = htons(PCC);
+		srand.sin_port = htons(PCF);
+
+		while(1)
+		{
+			if((ncmd = accept(cmd, (struct sockaddr *)&scmd, &lcmd)) < 0)
+			{
+				perror("Not accepting cmd");
+				exit(0);
+			}
+			else
+				printf("cmd accepting.....\n");
+
+			nrand = accept(rand, (struct sockaddr *)&srand, &lrand);
+				
+			if(nrand < 0)
+			{
+				perror("Not accepting rand");
+				exit(0);
+			}
+			else
+				printf("rand accepting.....\n");
+
+			pid = fork();
+
+			if(pid == 0)
+			{
+				close(cmd);
+				close(data);
+				close(rand);
+
+				fun(ncmd,nrand);
+
+				exit(0);
+			}
+			else
+			{
+				close(ncmd);
+				close(nrand);
+			}
+		}
+
+		return 0;
+}
